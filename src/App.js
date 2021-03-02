a0cfa37607fcb70ab4343ca911406cebbd312876
maklad42 <maklad42+git@gmail.com>
2021-03-02 23:18:34 +0900

    Add more info to the json file and moove it to the assets folder. 210302

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import sets from './assets/gearsets.json';
 import './App.scss';
 import { Banner } from './components/Banner';
 import { Gearset } from './components/Gearset';
 
 function App() {
+  const gear = sets.gearsets;
+  console.log(gear);
+
   return (
     <div className="App">
       <Banner />
-      <main className="main">
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-        <Gearset />
-      </main>
+      <main className="main">test</main>
     </div>
   );
 }
