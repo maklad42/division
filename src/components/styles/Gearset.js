f7ec96e56cbdd21574a2ffec8008709392d64caa
maklad42 <maklad42+git@gmail.com>
2021-03-04 22:53:33 +0900

    Refine the styles for the Gearset component, improving layout and colors. 210304

--- src/components/styles/Gearset.js
+++ src/components/styles/Gearset.js
@@ -3,15 +3,14 @@ import styled from 'styled-components/macro';
 export const Container = styled.div`
   width: 90%;
   position: relative;
-  height: 90%;
-  background-color: rgba(100, 200, 120, 0.1);
   color: #11dd11;
 `;
 
 export const Title = styled.div`
   background-color: rgba(200, 200, 200, 0.1);
-  color: #113333;
-  font-size: 2rem;
+  color: #dddddd;
+  text-shadow: 2px 2px 4px #222222;
+  font-size: 1.8rem;
   padding: 0.2rem;
 `;
 
@@ -19,6 +18,11 @@ export const Items = styled.div`
   border-top: solid 2px green;
 `;
 
-export const Item = styled.div``;
+export const Item = styled.div`
+  background-color: rgba(70, 102, 73, 0.2);
+  margin: 7px 0;
+  line-height: 1.5rem;
+  color: #bbc4bc;
+`;
 
 export const Check = styled.div``;
