d1a1cae938ae73155365c3948342762eb841b653
maklad42 <maklad42+git@gmail.com>
2021-03-09 23:12:56 +0900

    Add title and image to the header. Need to improve the font and shadow of the text. 210309

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ import { Container, Logo, Title, Menu } from './styles/Banner';
 export function Banner({ classes }) {
   return (
     <Container className={classNames('Container', classes)}>
-      <h1>The Division: Gear Sets</h1>
+      <div className="title">The Division: Gear Sets</div>
     </Container>
   );
 }
--- src/components/styles/Banner.js
+++ src/components/styles/Banner.js
@@ -2,7 +2,18 @@ import styled from 'styled-components';
 
 export const Container = styled.header`
   height: 150px;
-  margin-top: -22px;
   border-bottom: solid 2px #11ff11;
   background-color: #3f3f3f;
+  background-image: url('../images/logo.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: right;
+
+  .title {
+    position: relative;
+    line-height: 150px;
+    font-size: 4rem;
+    color: #ee7433;
+    text-shadow: 2px 2px 2px #686059;
+  }
 `;
